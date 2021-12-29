fe3298ce999a277ba5d5b61c46ec71fe6263d97c
wassim93 <wassim.benhassen@esprit.tn>
2021-12-29 23:44:24 +0000

    fetch data from api added

--- src/context.js
+++ src/context.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: "track1" } },
-      { track: { track_name: "track2" } },
-    ],
+    track_list: [],
     heading: "top 10 songs",
   };
+
+  componentDidMount() {
+    console.log(process.env.API_KEY);
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
