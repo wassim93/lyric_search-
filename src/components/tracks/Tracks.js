06d8338c0de3f35b5b6a02772f2d0586a438a648
wassim93 <wassim.benhassen@esprit.tn>
2022-01-02 00:23:38 +0000

    adding track component list

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,16 +1,27 @@
+import react from "react";
 import React from "react";
 import { Consumer } from "../../context";
 import Spinner from "../layout/Spinner";
+import Track from "./Track";
 const Tracks = () => {
   return (
     <Consumer>
       {(value) => {
-        console.log(value);
+        // console.log(value);
         const { track_list, heading } = value;
         if (track_list.length === 0 || track_list === undefined) {
           return <Spinner />;
         } else {
-          return <h1>Tracks</h1>;
+          return (
+            <React.Fragment>
+              <h3 className="text-center mb-4">{heading}</h3>
+              <div className="row">
+                {track_list.map((item) => (
+                  <Track key={item.track.track_id} track={item.track} />
+                ))}
+              </div>
+            </React.Fragment>
+          );
         }
       }}
     </Consumer>
